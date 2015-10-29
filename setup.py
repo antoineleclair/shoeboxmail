8bd253fe91c76c27665556b9c23bee47d97e413c
Antoine Leclair <antoineleclair@gmail.com>
2015-10-29 13:10:26 -0400

    Add python package and Dockerfile

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    ]
+
+setup(name='shoeboxsmtp',
+    version='0.0',
+    description='shoeboxsmtp',
+    classifiers=[
+    "Programming Language :: Python",
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='',
+    install_requires=requires,
+    packages=['shoeboxsmtp'],
+    package_dir={'shoeboxsmtp': 'shoeboxsmtp'},
+    zip_safe=False,
+    )
