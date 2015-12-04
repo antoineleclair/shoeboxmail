432965b33d438fd48e0ea4d862a8c32e593abf9f
Antoine Leclair <antoineleclair@gmail.com>
2015-12-03 20:48:19 -0500

    Bump version to 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='shoeboxmail',
-    version='0.0',
+    version='0.1',
     description='shoeboxmail',
     classifiers=[
     "Programming Language :: Python",
